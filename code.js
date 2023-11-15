71bdb48a7283f05a9b5d5acd86ce4042c44926a5
Aidan Giraldo <agirald1@uwyo.edu>
2023-11-15 15:09:48 -0700

    Start on code.js

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function depthFirstSearch(graph, startNode, targetNode) {
+    const visited = []; 
+    const path = []; 
+
+    while (visited.length < graph.length)
+    {
+        if (startNode == targetNode)
+        {
+            path.push(startNode); 
+            return path; 
+        }
+
+        else 
+        {
+            visited.push(startNode)
+            
+            for (var i = 0; i < graph.length; i++)
+            {
+                var row = graph[i]; 
+                depthFirstSearch(row, row[i], targetNode); 
+            }
+        }
+    }
     return [];
 }
